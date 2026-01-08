59e46eb09c7fc1deaf77f0c4b5692ef819ffa07c
jakub <jacob_the_liar@tlen.pl>
2026-01-08 15:07:32 +0100

    feat: initialize Spring Boot project with support for Neo4j
    Includes essential configurations, Maven wrapper, `.gitignore`, and `.gitattributes`.

---
+++ src/main/java/pl/jacobit/fraud/FraudDetectionNeo4jApplication.java
@@ -0,0 +1,13 @@
+package pl.jacobit.fraud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudDetectionNeo4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FraudDetectionNeo4jApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/jacobit/fraud/FraudDetectionNeo4jApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jacobit.fraud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionNeo4jApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
